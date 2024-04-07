6eec6ab0fdcd72e7bd1dd12bac1673fac590766d
Bob Park <hwpark11@kakao.com>
2024-04-07 21:36:57 +0900

    feat: intercepting route 추가

--- src/app/(afterLogin)/template.tsx
+++
@@ -1,8 +0,0 @@
-// 페이지에서 공통적으로 먼가 할때
-// ex:) google analytics 같은거?
-// 페이지가 넘나들을때마다 새롭게 component 가 mount 된다고 생각하면 됨
-export default function Template({ children }: { children: React.ReactNode }) {
-  console.log('afterl login template.');
-
-  return <div>{children}</div>;
-}
--- src/app/(beforeLogin)/i/flow/login/page.tsx
+++ src/app/(beforeLogin)/i/flow/login/page.tsx
@@ -1,3 +1,5 @@
-export default function Login() {
-  return <div>login</div>;
+import LoginForm from '@/components/user/LoginForm';
+
+export default function LoginPage() {
+  return <div>로그인 모달</div>;
 }
